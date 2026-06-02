d495ec7f8528c41f335de0592e733eb61c31a95a
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-06-02 15:32:36 +0700

    kondisi jika terjadi error di dalam try

--- exceptionstandart.cpp
+++ exceptionstandart.cpp
@@ -13,4 +13,6 @@ int main()
         ///memanggil array element ke-5 yang berarti akan terjadi error
         cout << data.at(5) << endl;
     }
+    //kondisi jika terjadi error di dalam try
+    catch (exception& e)
 };

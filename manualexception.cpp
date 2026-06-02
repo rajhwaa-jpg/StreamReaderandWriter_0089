e8e92cc5f04a08b1a273cfb7eb1b651e8a239ad4
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-06-02 16:30:25 +0700

    melemparkan sebuah interger maka

--- manualexception.cpp
+++ manualexception.cpp
@@ -2,4 +2,13 @@
 using namespace std;
 
 int main()
-{}
+{
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "selamat belajar di prodi ti umy" << endl;
+        //throw 0.5 //melemparkan sebuah integer maka
+        cout << "pernyataan tidak akan di tampilkan" << endl;
+    }
+    
+}

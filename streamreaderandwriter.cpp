696f5da4aff54211f99cf6b65e59361c2f97730b
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-06-02 16:46:50 +0700

    implement file writing and reading functionality

--- streamreaderandwriter.cpp
+++ streamreaderandwriter.cpp
@@ -10,4 +10,43 @@ int main()
 
     //membuka file dalam mode menulis
     ofstream outfile;
+    //menunjukan ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk meulis file
+    while (true)
+    {
+        cout << "- ";
+        //,endapat setiap karakter dalam satu baris
+        getline(cin, baris);
+        //kondisi false jika baris hanya berisi karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari variable baris ke dalamfile
+        outfile << baris << endl;
+    }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk file dalam mode membaca
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= membuka dan membaca file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai di loop
+        infile.close(); 
+    }
+    //kondisi jika file tidak ada
+    else cout << "unable to open file";
+    return 0;
 }

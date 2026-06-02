4007c6c1144301563b9d8c54717c0d012671dab8
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-06-02 15:30:02 +0700

    deklarasi array data dengan panjang index 3

--- exceptionstandart.cpp
+++ exceptionstandart.cpp
@@ -7,4 +7,9 @@ int main()
 {
     cout <<"awal program" << endl; //penanda 1
     try
+    {
+        //deklarasi array data dengan panjang index 3
+        array <int, 3> data = { 1, 2, 3 };
+        
+    }
 };
